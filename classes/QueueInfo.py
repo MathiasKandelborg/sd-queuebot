a2c250c3be569f826ce127c8fafca207b0a7270b
NeonRayTracer <33042011+MathiasKandelborg@users.noreply.github.com>
2024-02-18 16:39:49 +0100

    Use proper types in QueueInfo

--- classes/QueueInfo.py
+++ classes/QueueInfo.py
@@ -1,13 +1,14 @@
 from dataclasses import dataclass, field
+from disnake import User
 
 import disnake
 
 
 @dataclass
 class QueueInfo:
-    queue: list[disnake.user] = field(default_factory=list)
-    priority: list[disnake.user] = field(default_factory=list)
-    priorityUsers: list[disnake.user] = field(default_factory=list)
+    queue: list[User] = field(default_factory=list)
+    priority: list[User] = field(default_factory=list)
+    priorityUsers: list[User] = field(default_factory=list)
     queueEnabled: bool = True
     request: bool = False
     button_locations: list[disnake.TextChannel.id] = field(default_factory=list)
